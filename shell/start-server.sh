1f9fef8f4ad49b763d0c469b5baf5421ea61e9d6
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-05-13 11:21:16 +0900

    Fixed gitignore

---
+++ shell/start-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Waiting for mysql to start..."
+until mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &> /dev/null
+do
+    sleep 1
+done
+
+go build -o main && ./main
