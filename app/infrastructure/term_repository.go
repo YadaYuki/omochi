6b291d3a4b24f3abb1c9c3e74962976c4e7202ac
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-05-20 00:00:36 +0900

    Add NewTermRepository

--- app/infrastructure/term_repository.go
+++ app/infrastructure/term_repository.go
@@ -5,3 +5,7 @@ import "github.com/YadaYuki/omochi/app/ent"
 type TermRepository struct {
 	db *ent.Client
 }
+
+func NewTermRepository(db *ent.Client) *TermRepository {
+	return &TermRepository{db: db}
+}
