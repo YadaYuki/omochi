3e0db3baf0ca43e8bd4825361bc08032217b1f13
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-05-30 19:37:30 +0900

    Add InvertedIndexCompressed

--- app/domain/entities/invert_index.go
+++ app/domain/entities/invert_index.go
@@ -18,3 +18,11 @@ type InvertIndexCreate struct {
 	TermId      uuid.UUID  `json:"term_id"` // REVIEW: TermCreate may be better ?
 	PostingList *[]Posting `json:"posting_list"`
 }
+
+type InvertedIndexCompressed struct {
+	Uuid                  uuid.UUID `json:"uuid"`
+	TermId                uuid.UUID `json:"term_id"`
+	PostingListCompressed []byte    `json:"posting_list_compressed"`
+	CreatedAt             time.Time `json:"created_at"`
+	UpdatedAt             time.Time `json:"updated_at"`
+}
