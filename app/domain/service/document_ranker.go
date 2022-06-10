ad88c685c144f461d9064e4fd5544eaa018313ef
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-06-11 01:10:44 +0900

    Add Document Ranker

---
+++ app/domain/service/document_ranker.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"context"
+
+	"github.com/YadaYuki/omochi/app/domain/entities"
+	"github.com/YadaYuki/omochi/app/errors"
+)
+
+type DocumentRanker interface {
+	SortDocumentByScore(ctx context.Context, query string, docs *[]entities.DocumentDetail) (*[]entities.DocumentDetail, *errors.Error)
+	calculateDocumentScore(ctx context.Context, query string, docs *[]entities.DocumentDetail) (float64, *errors.Error)
+}
+
+// type DocumentRanker[T entities.Document | entities.DocumentDetail] interface {
+// 	SortDocumentByScore(ctx context.Context, query T, docs *[]T) (*[]T, *errors.Error)
+// 	calculateDocumentScore(ctx context.Context, query string, docs *[]T) (float64, *errors.Error)
+// }
