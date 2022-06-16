17dfebda3d2b0b23c3135e01f14226322bfee5b2
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-06-16 13:02:33 +0900

    Add Create Posting

--- app/domain/entities/posting.go
+++ app/domain/entities/posting.go
@@ -1,8 +1,10 @@
 package entities
 
-import "github.com/google/uuid"
-
 type Posting struct {
-	DocumentRelatedId   uuid.UUID `json:"document_related_id"`
-	PositionsInDocument []int     `json:"positions_in_document"`
+	DocumentRelatedId   int64 `json:"document_related_id"`
+	PositionsInDocument []int `json:"positions_in_document"`
+}
+
+func NewPosting(documentRelatedId int64, positionsInDocument []int) *Posting {
+	return &Posting{DocumentRelatedId: documentRelatedId, PositionsInDocument: positionsInDocument}
 }
--- app/infrastructure/indexer/indexer.go
+++ app/infrastructure/indexer/indexer.go
@@ -2,32 +2,51 @@ package indexer
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/YadaYuki/omochi/app/domain/entities"
-	"github.com/YadaYuki/omochi/app/ent"
+	"github.com/YadaYuki/omochi/app/domain/repository"
+	"github.com/YadaYuki/omochi/app/domain/service"
 	"github.com/YadaYuki/omochi/app/errors"
-	"github.com/YadaYuki/omochi/app/infrastructure/persistence/entdb"
 	"github.com/YadaYuki/omochi/app/infrastructure/transaction/wrapper"
 )
 
 type Indexer struct {
 	transactionWrapper *wrapper.EntTransactionWrapper
+	documentRepository repository.DocumentRepository
+	tokenizer          service.Tokenizer
 }
 
-func NewIndexer(wrapper *wrapper.EntTransactionWrapper) *Indexer {
+func NewIndexer(wrapper *wrapper.EntTransactionWrapper, documentRepository repository.DocumentRepository, tokenizer service.Tokenizer) *Indexer {
 	return &Indexer{transactionWrapper: wrapper}
 }
 
-func (indexer *Indexer) RegisterDocument(ctx context.Context, document *entities.Document) (*[]entities.DocumentDetail, *errors.Error) {
+func (i *Indexer) RegisterDocument(ctx context.Context, document *entities.Document) (*[]entities.DocumentDetail, *errors.Error) {
 
-	indexer.transactionWrapper.WithTx(ctx, func(transactionClient *ent.Client) error { // TODO: entの知識が入るのは微妙
-		termEntRepository := entdb.NewTermEntRepository(transactionClient)
-		documentEntRepository := entdb.NewTermEntRepository(transactionClient)
-		fmt.Println(termEntRepository, documentEntRepository)
-		return nil
-	})
+	// Create Invert Index from document
+	tokenizedContent, tokenizeErr := i.tokenizer.Tokenize(ctx, document.Content)
+	if tokenizeErr != nil {
+		return nil, errors.NewError(tokenizeErr.Code, tokenizeErr.Error())
+	}
+	document.TokenizedContent = make([]string, len(*tokenizedContent))
+	for i, term := range *tokenizedContent {
+		document.TokenizedContent[i] = term.Word
+	}
+	// TODO:Transaction
+	documentDetail, documentCreateErr := i.documentRepository.CreateDocument(ctx, document)
+	if documentCreateErr != nil {
+		return nil, errors.NewError(documentCreateErr.Code, documentCreateErr.Error())
+	}
+	documentId := documentDetail.Id
+
+	wordToPostingMap := make(map[string]*entities.Posting)
+	for position, word := range document.TokenizedContent {
+		if _, ok := wordToPostingMap[word]; ok {
+			wordToPostingMap[word].PositionsInDocument = append(wordToPostingMap[word].PositionsInDocument, position)
+		} else {
+			positionsInDocument := []int{position}
+			wordToPostingMap[word] = entities.NewPosting(documentId, positionsInDocument)
+		}
+	}
 
 	return nil, nil
-
 }
