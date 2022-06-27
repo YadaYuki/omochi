75fd092a0bf99500f951aa655df5c17b9bf2cb93
YadaYuki <yada.yuki@fuji.waseda.jp>
2022-06-27 23:28:07 +0900

    Add IndexingDocumentWithTx

--- pkg/infrastructure/indexer/entindexer/ent_indexer.go
+++ pkg/infrastructure/indexer/entindexer/ent_indexer.go
@@ -1,13 +1,40 @@
 package entindexer
 
 import (
+	"context"
+
+	"github.com/YadaYuki/omochi/pkg/domain/entities"
 	"github.com/YadaYuki/omochi/pkg/ent"
+	"github.com/YadaYuki/omochi/pkg/errors"
+	"github.com/YadaYuki/omochi/pkg/infrastructure/compresser"
+	"github.com/YadaYuki/omochi/pkg/infrastructure/indexer"
+	"github.com/YadaYuki/omochi/pkg/infrastructure/persistence/entdb"
+	"github.com/YadaYuki/omochi/pkg/infrastructure/tokenizer/eng"
+	"github.com/YadaYuki/omochi/pkg/infrastructure/transaction/wrapper"
 )
 
 type EntIndexer struct {
 	db *ent.Client
+	t  *wrapper.EntTransactionWrapper
 }
 
-func NewEntIndexer(db *ent.Client) *EntIndexer {
-	return &EntIndexer{db: db}
+func NewEntIndexer(db *ent.Client, t *wrapper.EntTransactionWrapper) *EntIndexer {
+	return &EntIndexer{db: db, t: t}
+}
+
+// IndexingDocumentWithTx is a function for indexing a document with RDB Transaction.
+func (entIndexer *EntIndexer) IndexingDocumentWithTx(ctx context.Context, document *entities.DocumentCreate) *errors.Error {
+
+	if err := entIndexer.t.WithTx(ctx, entIndexer.db, func(transactionClient *ent.Client) *errors.Error {
+		documentRepository := entdb.NewDocumentEntRepository(transactionClient)
+		termRepository := entdb.NewTermEntRepository(transactionClient)
+		tokenizer := eng.NewEnProseTokenizer()
+		invertIndexCompresser := compresser.NewZlibInvertIndexCompresser()
+		indexer := indexer.NewIndexer(documentRepository, termRepository, tokenizer, invertIndexCompresser)
+		return indexer.IndexingDocument(ctx, document)
+
+	}); err != nil {
+		return err
+	}
+	return nil
 }
--- pkg/infrastructure/transaction/wrapper/transaction_wrapper.go
+++ pkg/infrastructure/transaction/wrapper/transaction_wrapper.go
@@ -5,24 +5,25 @@ import (
 	"fmt"
 
 	"github.com/YadaYuki/omochi/pkg/ent"
+	"github.com/YadaYuki/omochi/pkg/errors"
+	"github.com/YadaYuki/omochi/pkg/errors/code"
 )
 
 type EntTransactionWrapper struct {
-	db *ent.Client
 }
 
-func NewEntTransactionWrapper(db *ent.Client) *EntTransactionWrapper {
-	return &EntTransactionWrapper{db: db}
+func NewEntTransactionWrapper() *EntTransactionWrapper {
+	return &EntTransactionWrapper{}
 }
 
-func (m *EntTransactionWrapper) WithTx(ctx context.Context, fn func(transactionClient *ent.Client) error) error {
-	tx, err := m.db.Tx(ctx)
+func (m *EntTransactionWrapper) WithTx(ctx context.Context, db *ent.Client, fn func(t *ent.Client) *errors.Error) *errors.Error {
+	tx, err := db.Tx(ctx)
 	if err != nil {
-		return err
+		return errors.NewError(code.Unknown, err)
 	}
 	if err := fn(tx.Client()); err != nil {
 		if rollbackErr := tx.Rollback(); rollbackErr != nil {
-			return fmt.Errorf("rolling back transaction: %w", rollbackErr)
+			return errors.NewError(code.Unknown, fmt.Errorf("rolling back transaction: %w", rollbackErr))
 		}
 		return err
 	}
